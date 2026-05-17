1a32743c5b5135531e1fd3b34da88a66db1a82c9
oriphiel-hr <Dobri1Duh2Git3>
2026-05-17 13:10:11 +0200

    s

--- RegistarPoslovnihSubjekata/backend/src/sudregPromjeneDiff.js
+++ RegistarPoslovnihSubjekata/backend/src/sudregPromjeneDiff.js
@@ -6,6 +6,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 const { getPromjene } = require('./sudregApi');
 const { closeWriteStream } = require('./jsonlStream');
 
